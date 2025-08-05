5e2903117d39a678cc286d1022fc815553590bbd
cyller1983 <cylller@gmail.com>
2025-08-05 23:57:39 +0800

    Implement code changes to enhance functionality and improve performance

--- lesson6/lesson6_5.py
+++ lesson6/lesson6_5.py
@@ -4,6 +4,6 @@ from google import genai
 client = genai.Client()
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="天空為什麼是藍的?"
+    model="gemini-2.5-flash", contents="python可以用來分析股票嗎?"
 )
 print(response.text)
